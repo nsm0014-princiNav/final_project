026abda7dcc1a7a035ee4742b4b005a0f2349657
noahfireball1 <nsm0014@auburn.edu>
2022-11-29 17:43:17 -0600

    Parsed mat files and resaved them

--- main.m
+++ main.m
@@ -3,5 +3,18 @@ clc
 clear
 close all
 format shortg
+addpath("data\")
+%% Loading in .mat files from Truck data
+load("random.mat","random")
+load("static.mat","static")
+load("turn.mat","turn")
 
-%% Initialize code
+for i = length(turn.IMU.Time)
+
+    [p_eb_e, v_eb_e, C_b2e] = Mechanize_ECEF(f_ib_b, omega_ib_b, dt, old_PVA);
+    
+    old_PVA.pos = 1;
+    old_PVA.vel = 1;
+    old_PVA.C_b2e = 1;
+end
+
